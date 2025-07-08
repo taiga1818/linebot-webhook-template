54f2145ca7f527220d92dc0cfcaf9bd30e354e9b
taiga1818 <tkn.ks1818@gmail.com>
2025-07-09 08:17:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,5 @@ def handle_image(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
